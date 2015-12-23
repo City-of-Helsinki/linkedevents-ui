cc32332265fe75f93b6443ef3b403f80ad196a63
Ville Touronen <touronen@gmail.com>
2015-12-23 15:29:27 +0200

    Add base for translations

--- src/scripts/views/app/index.js
+++ src/scripts/views/app/index.js
@@ -3,7 +3,10 @@ require('!style!css!sass!src/assets/main.scss');
 
 import React from 'react';
 
-// Material-ui components
+// Translations
+import {IntlProvider} from 'react-intl';
+import translations from 'src/scripts/i18n';
+
 import Headerbar from 'src/scripts/components/header';
 
 // Material-ui theming
@@ -17,23 +20,31 @@ class App extends React.Component {
     }
 
     static childContextTypes = {
-        muiTheme: React.PropTypes.object
+        muiTheme: React.PropTypes.object,
+        // language: React.PropTypes.object,
+        // user: React.PropTypes.object
     }
 
     getChildContext() {
         return {
-            muiTheme: HelTheme
+            muiTheme: HelTheme,
+            // language: this.props.language,
+            // user: this.props.user
         }
     }
 
     render() {
+        //const locale = this.props.language;
+        let locale = 'en';
         return (
-            <div>
-                <Headerbar />
-                <div className="container">
-                    {this.props.children}
+            <IntlProvider locale={locale} messages={translations[locale] || {}}>
+                <div>
+                    <Headerbar />
+                    <div className="container">
+                        {this.props.children}
+                    </div>
                 </div>
-            </div>
+            </IntlProvider>
         )
     }
 }
