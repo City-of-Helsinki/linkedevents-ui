b020c6b8f057e56c075b859943c538b15f7497a5
Ville Touronen <touronen@gmail.com>
2016-02-11 16:27:54 +0200

    Fix dev server

--- dev_server/getSettings.js
+++ dev_server/getSettings.js
@@ -8,11 +8,11 @@ const defaults = {
     helsinkiAuthId: process.env.CLIENT_ID || 'this-is-mock',
     helsinkiAuthSecret: process.env.CLIENT_SECRET || 'this-is-mock',
     helsinkiTargetApp: process.env.TARGET_APP || 'linkedevents-ui',
-    sessionSecret: process.env.SESSION_SECRET || Date.now(),
+    sessionSecret: process.env.SESSIONSECRET || process.env.SESSION_SECRET,
     dev: true
 }
 
-const requiredKeys = ["helsinkiAuthId", "helsinkiAuthSecret", "helsinkiTargetApp"]
+const requiredKeys = ["helsinkiAuthId", "helsinkiAuthSecret", "helsinkiTargetApp", 'sessionSecret']
 
 export default function getOptions() {
     // Check configs
