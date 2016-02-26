50c1b4dc59a89c44f7e158ef45b8bb6da590e746
Ville Touronen <touronen@gmail.com>
2016-02-26 19:20:12 +0200

    Separate user events to separate store

--- src/actions/userEvents.js
+++ src/actions/userEvents.js
@@ -27,15 +27,32 @@ function makeRequest(user = {}, page, dispatch) {
 
 export const startFetching = createAction(constants.REQUEST_EVENTS);
 
+export function receiveUserEvents(json) {
+    return {
+        type: constants.RECEIVE_USER_EVENTS,
+        items: json.data,
+        receivedAt: Date.now()
+    }
+}
+
+export function receiveUserEventsError(error) {
+    return {
+        type: constants.RECEIVE_USER_EVENTS_ERROR,
+        error: error,
+        items: []
+    }
+}
+
 export function fetchUserEvents(user, page) {
     return (dispatch) => {
         dispatch(startFetching);
         makeRequest(user, page, dispatch).then(function (response) {
             if (response.status >= 400) {
-                dispatch(receiveEvents({
-                    error: 'API Error ' + response.status}));
+                dispatch(receiveUserEventsError({
+                    error: 'API Error ' + response.status
+                }));
             }
-            response.json().then(json => dispatch(receiveEvents(json)));
+            response.json().then(json => dispatch(receiveUserEvents(json)));
         })
         .catch(e => {
             // Error happened while fetching ajax (connection or javascript)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import user from './user.js'
 import editor from './editor.js'
 import images from './images.js'
 import app from './app.js'
+import userEvents from './userEvents.js'
 
 export default {
     events: events,
     user: user,
     editor: editor,
     images: images,
-    app: app
+    app: app,
+    userEvents: userEvents
 }
