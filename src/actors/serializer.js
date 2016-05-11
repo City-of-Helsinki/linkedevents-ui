498eacc3cb30593f7982e2f94e9c7da0eb904650
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-05-11 18:15:12 +0300

    Reporting with JWT token's expiration time

--- src/actors/serializer.js
+++ src/actors/serializer.js
@@ -7,10 +7,14 @@ let arg = null;
 
 window.ARG = arg;
 
+var jwtDecode = require('jwt-decode');
+
 export default (store) => {
     const state = _.cloneDeep(_.omit(store.getState(), ['userEvents']));
     if (state.user) {
+        let token = jwtDecode(state.user.token);
         state.user.token = null; // JWT token is of not to be saved into Sentry
+        state.user.exp = token.exp;
     }
     window.ARG = state;
 }
