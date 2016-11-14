67f2f38b6b855b1e3ae605385b0228976004d7b1
Tomi Sjöblom <tomi.sjoblom@siili.com>
2016-11-14 14:19:20 +0200

    Align startdate and enddate to same row

--- src/components/FormFields/index.js
+++ src/components/FormFields/index.js
@@ -114,8 +114,14 @@ class FormFields extends React.Component {
                 </FormHeader>
                 <div className="row">
                     <div className="col-sm-6">
-                        <HelDateTimeField validationErrors={validationErrors['start_time']} defaultValue={values['start_time']} ref="start_time" name="start_time" label="event-starting-datetime" />
-                        <HelDateTimeField validationErrors={validationErrors['end_time']} defaultValue={values['end_time']} ref="end_time" name="end_time" label="event-ending-datetime" />
+                        <div className="row">
+                            <div className="col-xs-12 col-md-6">
+                                <HelDateTimeField validationErrors={validationErrors['start_time']} defaultValue={values['start_time']} ref="start_time" name="start_time" label="event-starting-datetime" />
+                            </div>
+                            <div className="col-xs-12 col-md-6">
+                                <HelDateTimeField validationErrors={validationErrors['end_time']} defaultValue={values['end_time']} ref="end_time" name="end_time" label="event-ending-datetime" />
+                            </div>
+                        </div>
                     </div>
                     <SideField>
                         <div className="tip">
