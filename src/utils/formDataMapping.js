5862b37c7769249adf297798a502e32e153ceb33
eetuseta <eetu.kaivola@siili.com>
2016-11-14 12:13:35 +0200

    removed useless export that crashes webpack

--- src/utils/formDataMapping.js
+++ src/utils/formDataMapping.js
@@ -131,7 +131,7 @@ function mapUIDataToAPIFormat(values) {
     */
 }
 
-export function mapAPIDataToUIFormat(values) {
+function mapAPIDataToUIFormat(values) {
     if(!values) {
         return {}
     }
