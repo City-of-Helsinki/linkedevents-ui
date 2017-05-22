7e06a77417653181d871262f599aebd7ebc7ef7d
Joni Rämö <joni.ramo.91@gmail.com>
2017-05-22 17:58:37 +0300

    Do validations correctly (#219)

--- src/reducers/editor.js
+++ src/reducers/editor.js
@@ -58,7 +58,7 @@ function update(state = initialState, action) {
         let validationErrors = Object.assign({}, state.validationErrors)
         // If there are validation errors, check if they are fixed
         if (_.keys(state.validationErrors).length > 0) {
-            validationErrors = doValidations(newValues, state.contentLanguages, state.validateFor)
+            validationErrors = doValidations(newValues, state.contentLanguages, state.validateFor || constants.PUBLICATION_STATUS.PUBLIC)
         }
 
         return Object.assign({}, state, {
