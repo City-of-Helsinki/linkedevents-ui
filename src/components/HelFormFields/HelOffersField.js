08447b2f1b5342857a02f556a19d71cd204919c9
Joni Rämö <joni.ramo@nordsoftware.com>
2017-05-27 11:47:55 +0300

    Connect NewOffer component, rename state variable

--- src/components/HelFormFields/HelOffersField.js
+++ src/components/HelFormFields/HelOffersField.js
@@ -3,11 +3,13 @@ import React from 'react'
 import { FormattedMessage, injectIntl } from 'react-intl'
 import MultiLanguageField from './MultiLanguageField'
 import HelCheckbox from './HelCheckbox'
+import NewOffer from './NewOffer'
 import './HelOffersField.scss'
 
 import { RaisedButton } from 'material-ui'
 
 import {connect} from 'react-redux'
+import { addOffer } from 'src/actions/editor.js'
 
 import ValidationPopover from 'src/components/ValidationPopover'
 
@@ -22,7 +24,7 @@ class HelOffersField extends React.Component {
       super(props);
       this.state = {
           values: this.props.defaultValue,
-          freeEvent: false
+          isFree: true
       };
     }
 
@@ -32,27 +34,34 @@ class HelOffersField extends React.Component {
         }
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.defaultValue && nextProps.defaultValue !== this.state.values) {
+            this.setState({ values: nextProps.defaultValue })
         }
     }
 
+    setIsFree(e, value) {
+        this.setState({ isFree: !this.state.isFree })
     }
 
     addNewOffer() {
         const obj = {
-            is_free: false
+            is_free: this.state.isFree
         }
-        this.context.dispatch(addOfferData(obj))
+        this.context.dispatch(addOffer(obj))
     }
 
     generateOffers() {
         const offers = []
         for (const key in this.props.defaultValue) {
             offers.push(
-                <div key={key} className="offer-fields">
-                    <MultiLanguageField defaultValue={this.props.defaultValue[key].info_url} ref={`info_url-${key}`} label="event-purchase-link" languages={this.props.languages} onBlur={e => this.onBlur(e)} validations={['isUrl']}  />
-                    <MultiLanguageField defaultValue={this.props.defaultValue[key].price} disabled={this.state.freeEvent} ref={`price-${key}`} label="event-price" languages={this.props.languages} onBlur={e => this.onBlur(e)} />
-                    <MultiLanguageField defaultValue={this.props.defaultValue[key].description} disabled={this.state.freeEvent} ref={`description-${key}`} label="event-price-info" languages={this.props.languages} multiLine={true} onBlur={e => this.onBlur(e)} />
-                </div>
+                <NewOffer
+                    key={key}
+                    offerKey={key}
+                    defaultValue={this.props.defaultValue[key]}
+                    languages={this.props.languages}
+                    isFree={this.state.isFree}
+                />
             )
         }
         return offers
@@ -68,14 +77,14 @@ class HelOffersField extends React.Component {
 
         return (
             <div className="offers-field">
-                <HelCheckbox defaultChecked={this.state.freeEvent} ref="is_free" label={<FormattedMessage id="is-free"/>} onChange={(e,v) => this.setIsFree(e,v)} />
+                <HelCheckbox defaultChecked={this.state.isFree} ref="is_free" label={<FormattedMessage id="is-free"/>} onChange={(e,v) => this.setIsFree(e,v)} />
                 <div className="offers">
                     { offerDetails }
                 </div>
                 <RaisedButton
                     style={buttonStyle}
                     primary={true}
-                    disabled={this.state.freeEvent}
+                    disabled={this.state.isFree}
                     onClick={ () => this.addNewOffer() }
                     label={<span><i className="material-icons">add</i> <FormattedMessage id="event-add-price" /></span>} />
             </div>
