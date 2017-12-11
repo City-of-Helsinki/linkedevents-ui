29cb97127814c1b015bc2eb955a2b045e097552b
Ville Koivunen <ville.koivunen@hel.fi>
2017-12-11 14:35:24 +0200

    In memoriam Tomi: move to using toml for configuration

--- server/getSettings.js
+++ server/getSettings.js
@@ -1,12 +1,15 @@
+// Do not use this to change settings in development (or production!)
+// instead in development use config_dev.toml in project root
+// and in production use environment variables
 const defaults = {
     port: 8080,
     // Used to sign the session cookie
-    // sessionSecret: 'development-secret',
+    sessionSecret: 'development-secret',
     // All four settings below need to match ones configured in SSO
-    // publicUrl: 'http://localhost:8080',
-    // helsinkiAuthId: 'this-is-mock',
-    // helsinkiAuthSecret: 'this-is-mock',
-    // helsinkiTargetApp: 'linkedevents-ui',
+    publicUrl: 'http://localhost:8080',
+    helsinkiAuthId: 'this-is-mock',
+    helsinkiAuthSecret: 'this-is-mock',
+    helsinkiTargetApp: 'linkedevents-ui'
 }
 
 const configKeys = ["port", "publicUrl", "helsinkiAuthId", "helsinkiAuthSecret", "helsinkiTargetApp", "sessionSecret"];
@@ -17,7 +20,7 @@ export default function getOptions() {
     nconf.env(configKeys)
     nconf.defaults(defaults)
     if(process.env.NODE_ENV == 'development') {
-        nconf.file({file: 'config_dev.json'})
+        nconf.file({file: 'config_dev.toml', format: require('nconf-toml')})
     }
 
     nconf.required(configKeys)
