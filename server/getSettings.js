f2d4d7f5249d92dd5897a6c98f4abdde06ddd7f4
Jimi <jiripim@gmail.com>
2018-06-06 14:14:58 +0300

    Centralize nconf/config handling

--- server/getSettings.js
+++ server/getSettings.js
@@ -1,26 +1,6 @@
-const defaults = {
-    port: 8080,
-    // Used to sign the session cookie
-    // sessionSecret: 'development-secret',
-    // All four settings below need to match ones configured in SSO
-    // publicUrl: 'http://localhost:8080',
-    // helsinkiAuthId: 'this-is-mock',
-    // helsinkiAuthSecret: 'this-is-mock',
-    // helsinkiTargetApp: 'linkedevents-ui',
-}
-
+const readConfig = require('../config/readConfig');
 const configKeys = ['port', 'publicUrl', 'helsinkiAuthId', 'helsinkiAuthSecret', 'helsinkiTargetApp', 'sessionSecret'];
 
 export default function getOptions() {
-    const nconf = require('nconf');
-
-    nconf.env(configKeys)
-    nconf.defaults(defaults)
-    if(process.env.NODE_ENV == 'development') {
-        nconf.file({file: 'config_dev.json'})
-    }
-
-    nconf.required(configKeys)
-
-    return nconf.get()
+    return readConfig(configKeys);
 }
