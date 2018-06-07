fde89307c22ca886f2687e378d87e50fa6834c7b
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-06-07 14:20:29 +0300

    Review comments implemented

--- src/components/HelFormFields/HelDateTimeField.js
+++ src/components/HelFormFields/HelDateTimeField.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react'
-//import HelTextField from './HelTextField.js'
+import HelTextField from './HelTextField.js'
 import HelDatePicker from './HelDatePicker.js'
 import HelTimePicker from './HelTimePicker.js'
 
