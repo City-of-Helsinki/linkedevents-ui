ede66f33857b85f456cd975e65bf7ec29117172e
Thanh Do <thanh.do@digia.com>
2018-11-28 12:26:26 +0200

    try to fix locale problem with snapshot
    Signed-off-by: Thanh Do <thanh.do@digia.com>

--- __mocks__/testReduxIntWrapper.js
+++ __mocks__/testReduxIntWrapper.js
@@ -1,10 +1,14 @@
-import {IntlProvider} from 'react-intl'
+import {IntlProvider, addLocaleData} from 'react-intl'
 import {MemoryRouter as Router} from 'react-router'
 import {Provider} from 'react-redux'
 import React from 'react'
+
 import translations from '../src/i18n'
 
 const ReduxIntWrappeComponent = (store, component, locale = 'fi') => {
+    const lang = require('react-intl/locale-data/' + locale)
+    addLocaleData(lang)
+
     return (
         <Provider store={store}>
             <IntlProvider locale={locale} messages={translations[locale]}>
