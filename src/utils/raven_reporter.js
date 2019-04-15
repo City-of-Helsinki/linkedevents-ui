c5b33d7638add399a4ee84d66afa986c0ca2ee0a
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2019-04-15 13:57:51 +0300

    Raven reporter structural fixing

--- src/utils/raven_reporter.js
+++ src/utils/raven_reporter.js
@@ -1,9 +1,16 @@
-
-var Raven = require('raven-js');
+var Raven = require("raven-js");
 if (appSettings.raven_id) {
     Raven.config(appSettings.raven_id).install();
 }
 
-export function report (data) {
-    Raven.captureMessage('reporting', {extra: {data: data}});
+export function report(data) {
+    Raven.captureMessage("reporting", {
+        extra: {
+            data: {
+                xdata: data,
+                user: data.user.emails[0].value,
+                message: data.debug_message
+            }
+        }
+    });
 }
