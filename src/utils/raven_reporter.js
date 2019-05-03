8207f2d1b9ac55673a40eaafa872f8034ae201ac
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2019-05-03 11:15:31 +0300

    Removing lint

--- src/utils/raven_reporter.js
+++ src/utils/raven_reporter.js
@@ -1,16 +1,16 @@
-var Raven = require("raven-js");
+var Raven = require('raven-js');
 if (appSettings.raven_id) {
     Raven.config(appSettings.raven_id).install();
 }
 
 export function report(data) {
-    Raven.captureMessage("reporting", {
+    Raven.captureMessage('reporting', {
         extra: {
             data: {
                 xdata: data,
                 user: data.user.emails[0].value,
-                message: data.debug_message
-            }
-        }
+                message: data.debug_message,
+            },
+        },
     });
 }
