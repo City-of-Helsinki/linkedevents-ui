64ab43ecf152c9ea2fa4c3c5de78c59b8f7f97cc
Kuisma Joentakanen <kuisma.joentakanen@digia.com>
2019-11-22 15:07:05 +0200

    Remove everything that is jquery related from the userImages action. Use Axios client for all API queries.

--- src/actions/userImages.js
+++ src/actions/userImages.js
@@ -1,28 +1,9 @@
 import {createAction} from 'redux-actions'
-// import fetch from 'isomorphic-fetch'
-import $ from 'jquery' // how do i the same thing in fetch?!? horrible docs
 import constants from '../constants'
 import {setData} from './editor'
 import {setFlashMsg} from './app'
 import {get as getIfExists} from 'lodash'
 import client from '../api/client'
-
-
-export function selectImage(image) {
-    return {
-        type: constants.SELECT_IMAGE_BY_ID,
-        img: image,
-    }
-}
-
-// TODO: Remove this with jquery
-function makeRequest(organization, pg_size) {
-    var url = `${appSettings.api_base}/image/?page_size=${pg_size}&publisher=${organization}`
-    if (appSettings.nocache) {
-        url += `&nocache=${Date.now()}`
-    }
-    return $.getJSON(url);
-}
 
 /**
  * Fetch images from the API.
@@ -66,24 +47,12 @@ export function fetchUserImages(organization, pageSize = 100, pageNumber = null)
             new Error(error);
         }
     };
-    
-    // Original
-    // return (dispatch) => {
-    //     dispatch(startFetching)
-    //     makeRequest(getIfExists(organization, 'organization', null), pageSize).done(response => {
-    //         dispatch(receiveUserImages(response))
-    //     }).fail(response => {
-    //         dispatch(setFlashMsg(getIfExists(response, 'detail', 'Error fetching images'), 'error', response))
-    //         dispatch(receiveUserImagesFail(response))
-    //     });
-    // }
 }
 
-// TODO: Remove this (and the reducer?) with jquery
-export function receiveUserImages(response) {
+export function selectImage(image) {
     return {
-        type: constants.RECEIVE_IMAGES,
-        items: response.data,
+        type: constants.SELECT_IMAGE_BY_ID,
+        img: image,
     }
 }
 
@@ -122,76 +91,32 @@ export function imageUploadComplete(json) {
     }
 }
 
-function getRequestBaseSettings(user, method = 'POST', imageId = null) {
-    let token = user ? user.token : ''
-    
-    let url = appSettings.api_base + '/image/'
-    if (imageId) {
-        url += imageId + '/'
-    }
-    
-    return {
-        'async': true,
-        'crossDomain': true,
-        'url': url,
-        'method': method,
-        'headers': {
-            'authorization': 'JWT ' + token,
-            'accept': 'application/json',
-        },
-        'processData': false,
-    }
-}
-
 export function postImage(formData, user, imageId = null) {
-    // return async (dispatch) => {
-    //     try {
-    //         const requestContentSettings = {
-    //             'mimeType': 'multipart/form-data',
-    //             'contentType': false,
-    //             'data': formData,
-    //         };
-    //
-    //     } catch (error) {
-    //
-    //     }
-    // }
+    return async (dispatch) => {
+        try {
+            // Use PUT when updating the existing image metadata, use POST when adding a new image.
+            const request = (imageId) ? await client.put(`image/${imageId}`, formData) : await client.post(`image/`, formData);
+
+            // Append updated image data to the form
+            dispatch(setData({'image': request}));
+            
+            dispatch(imageUploadComplete(request));
+            
+            dispatch(setFlashMsg(imageId ? 'image-update-success' : 'image-creation-success', 'success', request))
+        } catch (error) {
+            dispatch(setFlashMsg('image-creation-error', 'error', error));
     
-    return (dispatch) => {
-        const requestContentSettings = {
-            'mimeType': 'multipart/form-data',
-            'contentType': false,
-            'data': formData,
-        };
-
-        const baseSettings = imageId ? getRequestBaseSettings(user, 'PUT', imageId) : getRequestBaseSettings(user)
-
-        let settings = Object.assign({}, baseSettings, requestContentSettings)
-        return $.ajax(settings).done(response => {
-            //if we POST form-data, jquery won't parse the response
-            let resp = response
-            if(typeof(response) == 'string') {
-                resp = JSON.parse(response)
-            }
-
-            //creation success. set the newly created image as the val of the form
-            dispatch(setData({'image': resp}))
-            // and also set the preview image
-            dispatch(imageUploadComplete(resp))
-            // and flash a message
-            dispatch(setFlashMsg(imageId ? 'image-update-success' : 'image-creation-success', 'success', response))
-
-        }).fail(response => {
-            dispatch(setFlashMsg('image-creation-error', 'error', response))
-            dispatch(imageUploadFailed(response)) //this doesn't do anything ATM
-        })
+            dispatch(imageUploadFailed(error));
+            
+            new Error(error);
+        }
     }
 }
 
 export function deleteImage(selectedImage, user) {
     return async (dispatch) => {
         try {
-            const query = await client.delete(`image/${selectedImage.id}`);
+            const request = await client.delete(`image/${selectedImage.id}`);
     
             // Update form image value
             dispatch(setData({'image': null}));
