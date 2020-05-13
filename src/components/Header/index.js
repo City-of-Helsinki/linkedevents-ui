fc5e026d190fe8ce590122c0e2999e747e427e38
Jack Valley <Juuso.laakso@turku.fi>
2020-05-13 09:51:00 +0300

    Made changes to make this branch compatible with develop.
    appConfig, config_dev.json.example, Header/index.js, yarn.lock

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,34 +7,35 @@ import {connect} from 'react-redux'
 import {push} from 'react-router-redux'
 import {withRouter} from 'react-router'
 
-import {login as loginAction, logout as logoutAction} from 'src/actions/user.js'
+import {clearUserData as clearUserDataAction} from 'src/actions/user.js'
 import {setLocale as setLocaleAction} from 'src/actions/userLocale'
-import LanguageSelector from './LanguageSelector';
+
 import {FormattedMessage} from 'react-intl'
+
+// Material-ui Components
+import Select from 'react-select'
+import {Button, Drawer, Hidden, makeStyles, Toolbar} from '@material-ui/core'
+import {Add, Menu, Language, Person} from '@material-ui/icons'
+
+import {Link} from 'react-router-dom'
 import constants from '../../constants'
-//Updated Nav from Material UI to Reactstrap based on Open design
-import {Collapse, Navbar, NavbarToggler, Nav, NavbarBrand, Button} from 'reactstrap';
+
+import cityOfHelsinkiLogo from '../../assets/images/helsinki-logo.svg'
 import {hasOrganizationWithRegularUsers} from '../../utils/user'
 import {get} from 'lodash'
+import {HelMaterialTheme} from '../../themes/material-ui'
+import {HelSelectTheme, HelLanguageSelectStyles} from '../../themes/react-select'
 import moment from 'moment'
 import * as momentTimezone from 'moment-timezone'
+import helBrandColors from '../../themes/hel/hel-brand-colors'
+import userManager from '../../utils/userManager'
 
 const {USER_TYPE, APPLICATION_SUPPORT_TRANSLATION} = constants
 
 class HeaderBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false,
-            showModerationLink: false,
-        };
-    }
-
-    toggle() {
-        this.setState ({
-            isOpen: !this.state.isOpen,
-        });
+    state = {
+        navBarOpen: false,
+        showModerationLink: false,
     }
 
     componentDidMount() {
@@ -68,8 +69,37 @@ class HeaderBar extends React.Component {
         momentTimezone.locale(selectedOption.value)
     }
 
+    toggleNavbar = () => {
+        this.setState({navBarOpen: !this.state.navBarOpen});
+    }
+
+    getNavigateMobile = (navigate) => () => {
+        navigate();
+        this.toggleNavbar();
+    }
+
+    handleLoginClick = () => {
+        userManager.signinRedirect({
+            data: {
+                redirectUrl: window.location.pathname,
+            },
+            extraQueryParams: {
+                ui_locales: this.props.userLocale.locale, // set auth service language for user
+            },
+        });
+    }
+
+    handleLogoutClick = () => {
+        // clear user data in redux store
+        this.props.clearUserData();
+        
+        // passing id token hint skips logout confirm on tunnistamo's side
+        userManager.signoutRedirect({id_token_hint: this.props.auth.user.id_token});
+        userManager.removeUser();
+    }
+
     render() {
-        const {user, userLocale, routerPush, logout, login, location} = this.props
+        const {user, userLocale, routerPush, location} = this.props
         const {showModerationLink} = this.state
 
         const toMainPage = () => routerPush('/');
@@ -80,35 +110,52 @@ class HeaderBar extends React.Component {
         const isInsideForm = location.pathname.startsWith('/event/create/new');
 
         return (
-            <div className='main-navbar'>
-                <Navbar className='bar'>
-                    <NavbarBrand className="bar__logo" href="/" />
-                    <Nav className='ml-auto'>
-                        <div className='bar__login-and-language'>
-                            <div className='language-selector'>
-                                <LanguageSelector languages={this.getLanguageOptions()} userLocale={userLocale} changeLanguage={this.changeLanguage} />
-                            </div>
-                            {user ? (
-                                <Button
-                                    onClick={() => logout()}>
-                                    {user.displayName}
-                                </Button>
-                            ) : (
-                                <Button  
-                                    onClick={() => login()}>
-                                    <span className="glyphicon glyphicon-user"></span>
-                                    <FormattedMessage id='login' />
-                                </Button>
-                            )}
+            <div className="main-navbar">
+                <Toolbar className="helsinki-bar">
+                    <div className="helsinki-bar__logo">
+                        <Link to="/">
+                            <img src={cityOfHelsinkiLogo} alt="City Of Helsinki" />
+                        </Link>
+                    </div>
+                    <div className="helsinki-bar__login-button">
+                        <div className="helsinki-bar__language-button">
+                            <div className="language-selector">
+                                <Language className="language-icon"/>
+                                <Select
+                                    isClearable={false}
+                                    isSearchable={false}
+                                    value={{
+                                        label: userLocale.locale.toUpperCase(),
+                                        value: userLocale.locale,
+                                    }}
+                                    options={this.getLanguageOptions()}
+                                    onChange={this.changeLanguage}
+                                    styles={HelLanguageSelectStyles}
+                                    theme={HelSelectTheme}
+                                />
+                            </div>  
                         </div>
-                    </Nav>
-                </Navbar>
-        
-                <Navbar className="linked-events-bar" expand='lg'>
-                    <NavbarBrand className="linked-events-bar__logo" onClick={() => routerPush('/')}><FormattedMessage id={`linked-${appSettings.ui_mode}`} /></NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <div className="linked-events-bar__links">
+                        {user
+                            ? <Button
+                                style={{color: HelMaterialTheme.palette.primary.contrastText}}
+                                onClick={this.handleLogoutClick}
+                            >
+                                {user.displayName}
+                            </Button>
+                            : <Button
+                                style={{color: HelMaterialTheme.palette.primary.contrastText}}
+                                startIcon={<Person/>}
+                                onClick={this.handleLoginClick}
+                            >
+                                <FormattedMessage id="login"/>
+                            </Button>}
+                    </div>
+                </Toolbar>
+                
+                <Toolbar className="linked-events-bar">
+                    <div className="linked-events-bar__logo" onClick={() => routerPush('/')}><FormattedMessage id={`linked-${appSettings.ui_mode}`} /></div>
+                    <div className="linked-events-bar__links">
+                        <Hidden smDown>
                             <div className="linked-events-bar__links__list">
                                 <NavLinks
                                     showModerationLink={showModerationLink}
@@ -118,19 +165,36 @@ class HeaderBar extends React.Component {
                                     toModerationPage={toModerationPage}
                                 />
                             </div>
-                            <Nav className="ml-auto" navbar>
-                                {!isInsideForm && (
-                                    <Button
-                                        className="linked-events-bar__links__create-events"
-                                        onClick={() => routerPush('/event/create/new')}
-                                    >
-                                        <span className="glyphicon glyphicon-plus"></span>
-                                        <FormattedMessage id={`create-${appSettings.ui_mode}`}/>
-                                    </Button> )}
-                            </Nav>
+                        </Hidden>
+                        <div />
+                        <div className="linked-events-bar__links__mobile">
+                            {!isInsideForm && (
+                                <Button
+                                    variant="outlined"
+                                    className="linked-events-bar__links__create-events"
+                                    onClick={() => routerPush('/event/create/new')}
+                                    startIcon={<Add/>}
+                                >
+                                    <FormattedMessage id={`create-${appSettings.ui_mode}`}/>
+                                </Button>
+                            )}
+                            <Hidden mdUp>
+                                <Menu className="linked-events-bar__icon" onClick={this.toggleNavbar} />
+                                <Drawer anchor='right' open={this.state.navBarOpen} ModalProps={{onBackdropClick: this.toggleNavbar}}>
+                                    <div className="menu-drawer-mobile">
+                                        <NavLinks
+                                            showModerationLink={showModerationLink}
+                                            toMainPage={this.getNavigateMobile(toMainPage)}
+                                            toSearchPage={this.getNavigateMobile(toSearchPage)}
+                                            toHelpPage={this.getNavigateMobile(toHelpPage)}
+                                            toModerationPage={this.getNavigateMobile(toModerationPage)}
+                                        />
+                                    </div> 
+                                </Drawer>
+                            </Hidden>
                         </div>
-                    </Collapse>
-                </Navbar>
+                    </div>
+                </Toolbar>
             </div>
         )
     }
@@ -138,8 +202,8 @@ class HeaderBar extends React.Component {
 
 const NavLinks = (props) => {
     const {showModerationLink, toMainPage, toSearchPage, toHelpPage, toModerationPage} = props;
-    const moderationStyles = showModerationLink && (theme => ({
-
+    const moderationStyles = showModerationLink && makeStyles(theme => ({
+        root: {color: theme.palette.primary.main},
     }))()
 
     return (
@@ -170,28 +234,27 @@ NavLinks.propTypes = {
 // Adds dispatch to this.props for calling actions, add user from store to props
 HeaderBar.propTypes = {
     user: PropTypes.object,
-    login: PropTypes.func,
-    logout: PropTypes.func,
     routerPush: PropTypes.func,
     userLocale: PropTypes.object,
     setLocale: PropTypes.func,
     location: PropTypes.object,
+    navBarOpen: PropTypes.bool,
     showModerationLink: PropTypes.bool,
-    type: PropTypes.string,
-    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+    clearUserData: PropTypes.func,
+    auth: PropTypes.object,
 }
-
 
 const mapStateToProps = (state) => ({
     user: state.user,
     userLocale: state.userLocale,
+    auth: state.auth,
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    login: () => dispatch(loginAction()),
-    logout: () => dispatch(logoutAction()),
     routerPush: (url) => dispatch(push(url)),
     setLocale: (locale) => dispatch(setLocaleAction(locale)),
+    clearUserData: () => dispatch(clearUserDataAction()),
 })
 
+export {HeaderBar as UnconnectedHeaderBar};
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(HeaderBar))
