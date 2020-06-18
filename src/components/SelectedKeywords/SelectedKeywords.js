ab480c8247cdfd9ea845ff2d54d4875853c2be90
Tarja Mokko <58773830+tmokko@users.noreply.github.com>
2020-06-18 09:54:41 +0300

    removed material ui Chip (#59)
    replaced whit reactstrap badge

--- src/components/SelectedKeywords/SelectedKeywords.js
+++ src/components/SelectedKeywords/SelectedKeywords.js
@@ -1,42 +1,42 @@
+import './SelectedKeywords.scss'
 import PropTypes from 'prop-types';
 import React from 'react'
-import {Chip} from '@material-ui/core'
-import {HelMaterialTheme} from '../../themes/material-ui'
+// Removed Material-Ui/core since it's no longer in use.
+import {Badge, Button} from 'reactstrap';
 
-const containerStyles = {
-    margin: HelMaterialTheme.spacing(1, 0, 2),
+
+
+
+const SelectedKeywords = ({selectedKeywords,onDelete}) => {
+    
+   
+    return(
+        <div
+            className="keyword-chip-container"
+        >
+            {selectedKeywords.map((keyword, index) => (
+                <Badge
+                    className="keyword-chip-item"
+                    key={`keyword-${index}`}
+                    color="primary"
+                >{keyword.label}  
+                    <Button onClick={() => onDelete(keyword)} className="badge badge-primary">x</Button>
+                </Badge>
+            ))}
+        </div>
+    )
 }
-
-const chipStyles = {
-    margin: HelMaterialTheme.spacing(1, 1, 0, 0),
-}
-
-const SelectedKeywords = ({selectedKeywords, onDelete}) => (
-    <div
-        className="keyword-chip-container"
-        style={containerStyles}
-    >
-        {selectedKeywords.map((keyword, index) => (
-            <Chip
-                className="keyword-chip-item"
-                key={`keyword-${index}`}
-                color="primary"
-                label={keyword.label}
-                style={chipStyles}
-                onDelete={() => onDelete(keyword)}
-            />
-        ))}
-    </div>
-)
 
 SelectedKeywords.defaultProps = {
     selectedKeywords: [],
     onDelete: () => {},
+   
 }
 
 SelectedKeywords.propTypes = {
     selectedKeywords: PropTypes.array,
     onDelete: PropTypes.func,
+    
 }
 
 export default SelectedKeywords
