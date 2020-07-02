2e6237062c6c327f8f24c504e8db34b264f2bb65
Tarja Mokko <58773830+tmokko@users.noreply.github.com>
2020-07-02 15:19:11 +0300

    improved  keyword bages (#85)

--- src/components/SelectedKeywords/SelectedKeywords.js
+++ src/components/SelectedKeywords/SelectedKeywords.js
@@ -3,24 +3,20 @@ import PropTypes from 'prop-types';
 import React from 'react'
 // Removed Material-Ui/core since it's no longer in use.
 import {Badge, Button} from 'reactstrap';
-
-
-
-
 const SelectedKeywords = ({selectedKeywords,onDelete}) => {
-    
-   
     return(
         <div
             className="keyword-chip-container"
         >
             {selectedKeywords.map((keyword, index) => (
                 <Badge
+                    aria-hidden="true"
                     className="keyword-chip-item"
                     key={`keyword-${index}`}
                     color="primary"
                 >{keyword.label}  
-                    <Button onClick={() => onDelete(keyword)} className="badge badge-primary">x</Button>
+                    <Button onClick={() => onDelete(keyword)} className="badge badge-pill  badge-primary" 
+                        aria-hidden="true">&times;</Button>
                 </Badge>
             ))}
         </div>
@@ -30,13 +26,10 @@ const SelectedKeywords = ({selectedKeywords,onDelete}) => {
 SelectedKeywords.defaultProps = {
     selectedKeywords: [],
     onDelete: () => {},
-   
 }
-
 SelectedKeywords.propTypes = {
     selectedKeywords: PropTypes.array,
-    onDelete: PropTypes.func,
-    
+    onDelete: PropTypes.func,  
 }
 
 export default SelectedKeywords
